4eb8e4ef2074c8f8a495957ee5d5da006f5d2023
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-11-17 15:02:52 +0500

    added docker

--- internal/usecase/redis/redis.go
+++ internal/usecase/redis/redis.go
@@ -8,9 +8,9 @@ import (
 
 func NewRedisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Адрес Redis-сервера
-		Password: "",               // Пароль (если отсутствует, оставьте пустым)
-		DB:       0,                // Номер базы данных (по умолчанию 0)
+		Addr:     "redis:6379", // Адрес Redis-сервера
+		Password: "",           // Пароль (если отсутствует, оставьте пустым)
+		DB:       0,            // Номер базы данных (по умолчанию 0)
 	})
 
 	if err := rdb.Ping(context.Background()).Err(); err != nil {
